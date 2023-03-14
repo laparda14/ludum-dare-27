3a0dd8e7657433ae5b8e30023a1070c49eded8dc
laparda14 <120476892+laparda14@users.noreply.github.com>
2023-03-14 12:55:58 -0400

    Update health.js

--- components/health.js
+++ components/health.js
@@ -11,7 +11,7 @@ function health(amount) {
     .on('damaged', function(dmg) {
       this.health -= dmg
       if (!this.dead && this.health <= 0) {
-        this.dead = true
+        this.dead = false
         this.trigger('died')
       }
     })
