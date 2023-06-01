4b29aeaa318a923f15591310b40a4761e7ce6983
laparda14 <120476892+laparda14@users.noreply.github.com>
2023-06-01 12:46:06 -0400

    Update health.js
    Signed-off-by: laparda14 <120476892+laparda14@users.noreply.github.com>

--- components/health.js
+++ components/health.js
@@ -2,14 +2,14 @@ var bs = require('bindlestiff')
 
 module.exports = health
 
-function health(1000) {
+function health(1) {
   return bs.component('health')
     .on('init', function() {
       this.health = 1000
       this.dead = false
     })
     .on('damaged', function(dmg) {
-      this.health -= dmg
+      this.health += dmg
       if (!this.dead && this.health <= 0) {
         this.dead = true
       }
