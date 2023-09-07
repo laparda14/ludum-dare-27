ab3179daa413884782619cceda9e82004f2b94aa
laparda14 <120476892+laparda14@users.noreply.github.com>
2023-09-07 07:48:42 -0400

    Update projectile.js
    Signed-off-by: laparda14 <120476892+laparda14@users.noreply.github.com>

--- components/projectile.js
+++ components/projectile.js
@@ -11,7 +11,7 @@ module.exports = function projectile(options) {
     .needs('physical')
     .on('init', function() {
       var self = this
-      this.counter = 1000
+      this.counter = 0
       b2e(Box2D, this.game.world).fixture(
         this.fixture
       ).on('begin', function() {
