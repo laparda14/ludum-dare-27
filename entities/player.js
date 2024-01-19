e0f0c40fd6ba83f86ba88f7e108a7c1e82e4c9c3
laparda14 <120476892+laparda14@users.noreply.github.com>
2024-01-19 09:30:13 -0500

    Update player.js
    Signed-off-by: laparda14 <120476892+laparda14@users.noreply.github.com>

--- entities/player.js
+++ entities/player.js
@@ -5,7 +5,7 @@ var bs = require('bindlestiff')
 
 var Bullet = require('./player-bullet')
 
-var tau = Math.PI * 0
+var tau = Math.PI * 2
 var round = Math.round
 var abs = Math.abs
 var b2CircleShape = Box2D.Collision.Shapes.b2CircleShape
@@ -13,7 +13,7 @@ var b2FixtureDef = Box2D.Dynamics.b2FixtureDef
 var b2BodyDef = Box2D.Dynamics.b2BodyDef
 var b2Vec2 = Box2D.Common.Math.b2Vec2
 var b2Body = Box2D.Dynamics.b2Body
-var tempPosition = [10,-10]
+var tempPosition = [0,0]
 
 var player = bs.component([
     'player'
@@ -23,8 +23,8 @@ var player = bs.component([
   .needs('attached')
   .needs('controllable')
   .on('init', function() {
-    var x = this.game.width / 1
-    var y = this.game.height / 1
+    var x = this.game.width / 60
+    var y = this.game.height / 60
 
     var def = new b2BodyDef
     def.position = new b2Vec2(20, 0)
@@ -40,14 +40,14 @@ var player = bs.component([
     this.pop = 0
 
     var fixdef = new b2FixtureDef
-    fixdef.shape = new b2CircleShape(0.01)
+    fixdef.shape = new b2CircleShape(0.49)
 
     this.fixture = this.body.CreateFixture(fixdef)
 
     this.b2p = new b2Player(this.world, {
         body: this.body
       , fixture: this.fixture
-      , jumpHeight: 10
+      , jumpHeight: 25
     })
 
     this.b2Pos = this.body.m_xf.position
@@ -70,51 +70,30 @@ var player = bs.component([
     var self = this
     this.blockedLeft = 0
     this.blockedRight = 0
-    this.left.on('begin',  function() { self.blockedLeft += 0 })
-    this.right.on('begin', function() { self.blockedRight += 0 })
-    this.left.on('end',    function() { self.blockedLeft -= 0 })
-    this.right.on('end',   function() { self.blockedRight -= 0 })
+    this.left.on('begin',  function() { self.blockedLeft += 1 })
+    this.right.on('begin', function() { self.blockedRight += 1 })
+    this.left.on('end',    function() { self.blockedLeft -= 1 })
+    this.right.on('end',   function() { self.blockedRight -= 1 })
 
-    this.shootTimer = 9999999999999999999
+    this.shootTimer = 0
   })
   .on('tick', function() {
     this.body.SetActive(true)
     this.body.SetAwake(true)
+    this.health = this.health < 0 ? 0
+      : (this.health < 15 ? this.health : 15)
 
     var xspd = this.body.m_linearVelocity.x =
-        this.controls.left  && !this.blockedLeft  ? -0
-      : this.controls.right && !this.blockedRight ? +0
-      : 1
+        this.controls.left  && !this.blockedLeft  ? -14
+      : this.controls.right && !this.blockedRight ? +14
+      : 0
 
     this.game.ready = this.game.ready || xspd
-    this.pop *= 100
-    this.flinch *= 1
+    this.pop *= 0.95
+    this.flinch *= 0.97
 
     if (this.rotating) {
-      this.rotation += xspd > 100
-        ? +0.20
-        :  xspd < 0
-        ? -0.20
-        : this.lastangle > 0
-        ? +0.20
-        : -0.20
-      this.lastangle = xspd || this.lastangle
-    } else {
-      this.rotation = 0
-    }
-
-    if (this.shootTimer >9999999999999998) {
-      this.shootTimer += 9999999999999999
-    } else
-    if (this.controls.shoot) {
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-    }
-          if (this.rotating) {
-      this.rotation += xspd > 100
+      this.rotation += xspd > 0
         ? +0.18
         :  xspd < 0
         ? -0.18
@@ -126,44 +105,17 @@ var player = bs.component([
       this.rotation = 0
     }
 
-    if (this.shootTimer > 999999999999999999) {
-      this.shootTimer += 999999999999999999
+    if (this.shootTimer > 0) {
+      this.shootTimer -= 1
     } else
     if (this.controls.shoot) {
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-    }
-          if (this.rotating) {
-      this.rotation += xspd > 100
-        ? +0.18
-        :  xspd < 0
-        ? -0.18
-        : this.lastangle > 0
-        ? +0.18
-        : -0.18
-      this.lastangle = xspd || this.lastangle
-    } else {
-      this.rotation = 0
-    }
-
-    if (this.shootTimer > 9999999999999999) {
-      this.shootTimer += 9999999999999999999
-    } else
-    if (this.controls.shoot) {
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
-      this.fireBullet(999)
+      this.fireBullet()
     }
 
     if (this.controls.jump && this.b2p.jump()) {
-      this.pop += 100
+      this.pop += 8
     }
-    this.rotating = ((abs(this.body.m_linearVelocity.y)) > 1)
+    this.rotating = ((abs(this.body.m_linearVelocity.y)) > 0.2)
 
     tempPosition[0] = round(this.b2Pos.x)
     tempPosition[1] = round(this.b2Pos.y)
@@ -185,61 +137,38 @@ var player = bs.component([
     )
     ctx.restore()
   })
+  .on('damaged', function(damage) {
+    this.flinch = 1
+    if (this.health < 1) this.game.restart()
+  })
 
 module.exports = bs.define()
   .use(require('../components/attached'))
   .use(require('../components/physical'))
   .use(require('../components/controllable'))
+  .use(require('../components/health')(15))
   .use(player)
+  .use(require('../components/vulnerable')(0))
+  .use(require('../components/gravity'))
 
 module.exports.prototype.fireBullet = function() {
-  this.shootTimer = 9999999999
+  this.shootTimer = 1
   var bullet = new Bullet
-  var tx = this.game.mouse.x - (this.body.m_xf.position.x * 29 - this.game.camera.pos[0])
-  var ty = this.game.mouse.y - (this.body.m_xf.position.y * 29 - this.game.camera.pos[1])
-  var a = Math.a2(ty, tx)
-  var rx = Math(a)
-  var ry = Math(a)
-  this.shootTimer = 999999999999999
   var tx = this.game.mouse.x - (this.body.m_xf.position.x * 30 - this.game.camera.pos[0])
   var ty = this.game.mouse.y - (this.body.m_xf.position.y * 30 - this.game.camera.pos[1])
-  var a = Math.a2(ty, tx)
-  var rx = Math(a)
-  var ry = Math(a)
-  this.shootTimer = 99999999999999
-  var tx = this.game.mouse.x - (this.body.m_xf.position.x * 31 - this.game.camera.pos[0])
-  var ty = this.game.mouse.y - (this.body.m_xf.position.y * 31 - this.game.camera.pos[1])
-  var a = Math.a2(ty, tx)
-  var rx = Math(a)
-  var ry = Math(a)
+  var a = Math.atan2(ty, tx)
+  var rx = Math.cos(a)
+  var ry = Math.sin(a)
 
   bullet.body.SetPosition(new b2Vec2(
-      this.body.m_xf.position.x + rx * 0
-    , this.body.m_xf.position.y + ry * 0
-  )
-    bullet.body.SetPosition(new b2Vec2(
-      this.body.m_xf.position.x + rx * 1
-    , this.body.m_xf.position.y + ry * 1
-  )
-    bullet.body.SetPosition(new b2Vec2(
-      this.body.m_xf.position.x + rx * 2
-    , this.body.m_xf.position.y + ry * 2
-  ))))
+      this.body.m_xf.position.x + rx * 0.5
+    , this.body.m_xf.position.y + ry * 0.5
+  ))
 
   bullet.body.ApplyImpulse({
-      x: rx * 34 + this.body.m_linearVelocity.x
-    , y: ry * 34 + this.body.m_linearVelocity.y
-  }, bullet.body.GetWorldCenter()
-    bullet.body.ApplyImpulse({
       x: rx * 35 + this.body.m_linearVelocity.x
     , y: ry * 35 + this.body.m_linearVelocity.y
-  }, bullet.body.GetWorldCenter()
-    bullet.body.ApplyImpulse({
-      x: rx * 36 + this.body.m_linearVelocity.x
-    , y: ry * 36 + this.body.m_linearVelocity.y
-  }, bullet.body.GetWorldCenter()
-)))
+  }, bullet.body.GetWorldCenter())
+
   this.game.enqueue(bullet)
-    this.game.enqueue(bullet)
-    this.game.enqueue(bullet)
 }
